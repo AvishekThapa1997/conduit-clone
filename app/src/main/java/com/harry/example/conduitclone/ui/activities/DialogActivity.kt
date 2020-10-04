18dad2d33cfbe8941b413230f7b44c5445f9d2c4
avishekthapa1997@gmail.com <avishekthapa1997@gmail.com>
2020-10-04 17:45:09 +0530

    Default user avataar added in dialog activity

--- app/src/main/java/com/harry/example/conduitclone/ui/activities/DialogActivity.kt
+++ app/src/main/java/com/harry/example/conduitclone/ui/activities/DialogActivity.kt
@@ -10,38 +10,43 @@ import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.harry.example.conduitclone.R
-import com.harry.example.conduitclone.utility.drawable
 import kotlinx.android.synthetic.main.activity_dialog.*
 
 class DialogActivity : AppCompatActivity() {
+    private val glideListener : RequestListener<Drawable> by lazy {
+        object  : RequestListener<Drawable>{
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                stopShimmer()
+               return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                stopShimmer()
+                return false
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dialog)
         intent.getBundleExtra("bundle")?.apply {
-            Glide.with(applicationContext).load(getString("url", ""))
-                .listener(object : RequestListener<Drawable> {
-                    override fun onResourceReady(
-                        resource: Drawable?,
-                        model: Any?,
-                        target: Target<Drawable>?,
-                        dataSource: DataSource?,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        stopShimmer()
-                        return false
-                    }
-
-                    override fun onLoadFailed(
-                        e: GlideException?,
-                        model: Any?,
-                        target: Target<Drawable>?,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        stopShimmer()
-                        image.setImageDrawable(drawable(R.drawable.default_user_avataar))
-                        return false
-                    }
-                }).into(image)
+            val url = getString("url", "")
+            if (url.isNotEmpty() || url.isNotBlank()) {
+                Glide.with(applicationContext).load(url).listener(glideListener).into(image)
+            } else {
+                Glide.with(applicationContext).load(R.drawable.default_user_avataar).listener(glideListener).into(image)
+            }
         }
     }
 
